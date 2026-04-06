f5ee34d3b3ffe7d0c3207cd14d0a4c508ef41c95
organicwhale <organicwhale@gmail.com>
2026-04-06 20:51:32 +0900

    메시지 좌우 정렬 및 입력창 확대

--- client/script.js
+++ client/script.js
@@ -16,11 +16,26 @@ function joinRoom() {
   document.getElementById("setup").classList.add("hidden");
   document.getElementById("chatRoom").classList.remove("hidden");
 
-  socket.on('message', (data) => {
-    const chat = document.getElementById("chat");
-    chat.innerHTML += `<div><b>${data.nickname}:</b> ${data.message}</div>`;
-    chat.scrollTop = chat.scrollHeight;
-  });
+socket.on("message", (data) => {
+  const chat = document.getElementById("chat");
+
+  if (data.nickname === "시스템") {
+    chat.innerHTML += `<div class="system-message">${data.message}</div>`;
+  } else {
+    const messageClass = data.nickname === nickname ? "me" : "other";
+
+    chat.innerHTML += `
+      <div class="message-row ${messageClass}">
+        <div class="message-bubble">
+          <b>${data.nickname}</b><br>
+          ${data.message}
+        </div>
+      </div>
+    `;
+  }
+
+  chat.scrollTop = chat.scrollHeight;
+});
 }
 
 function sendMessage() {
